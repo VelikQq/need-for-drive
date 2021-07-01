97440c730964c12c10f3c01bc74d9c919577052c
VelikQq <romankazarov@yandex.ru>
2021-07-01 20:26:26 +0400

    Updates

---
+++ precache-manifest.26f895e4784ca7145cff03d167aaadc6.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e271a6a07b2972c954b9e48f73f2cf44",
+    "url": "/need-for-drive/index.html"
+  },
+  {
+    "revision": "5d2bbb70f309b2540838",
+    "url": "/need-for-drive/static/css/2.1be1aeea.chunk.css"
+  },
+  {
+    "revision": "37c30f94aa6888a1676d",
+    "url": "/need-for-drive/static/css/main.69a4e760.chunk.css"
+  },
+  {
+    "revision": "5d2bbb70f309b2540838",
+    "url": "/need-for-drive/static/js/2.100dc101.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/need-for-drive/static/js/2.100dc101.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37c30f94aa6888a1676d",
+    "url": "/need-for-drive/static/js/main.be2de892.chunk.js"
+  },
+  {
+    "revision": "d626b68bd1aca0cbff82",
+    "url": "/need-for-drive/static/js/runtime-main.7c67051a.js"
+  },
+  {
+    "revision": "514444866a5bbe0f2b042ff67a8a32d5",
+    "url": "/need-for-drive/static/media/Facebook_white.51444486.svg"
+  },
+  {
+    "revision": "7cc0558f6c8e98abbd6d3a31aec109fa",
+    "url": "/need-for-drive/static/media/Group.7cc0558f.svg"
+  },
+  {
+    "revision": "55529b248297a63ba84e516608241db3",
+    "url": "/need-for-drive/static/media/Helvetica-Regular.55529b24.ttf"
+  },
+  {
+    "revision": "48e5d7d2807c311019e25fcee8e1bab8",
+    "url": "/need-for-drive/static/media/Instagram_white.48e5d7d2.svg"
+  },
+  {
+    "revision": "be0b6f1f5c7ee3934104c0ed712c9c8a",
+    "url": "/need-for-drive/static/media/Map_Point.be0b6f1f.svg"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/need-for-drive/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/need-for-drive/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/need-for-drive/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/need-for-drive/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "b1d7b95a5c4b0b736d50c127a85c4574",
+    "url": "/need-for-drive/static/media/Telegram_white.b1d7b95a.svg"
+  },
+  {
+    "revision": "7f93b98434ed894eaaa7c49e72da4d31",
+    "url": "/need-for-drive/static/media/Vector.7f93b984.svg"
+  },
+  {
+    "revision": "727e5851432e590d0d0746da10a9d0cc",
+    "url": "/need-for-drive/static/media/bg_slider_0.727e5851.jpg"
+  },
+  {
+    "revision": "1ea0df98ab94212c7d40067067e9267d",
+    "url": "/need-for-drive/static/media/bg_slider_1.1ea0df98.jpg"
+  },
+  {
+    "revision": "def9cb41a4a0ecde8b8dee91b03a5bbd",
+    "url": "/need-for-drive/static/media/bg_slider_2.def9cb41.jpg"
+  },
+  {
+    "revision": "a532e517e172c94ef966ee63c190dee7",
+    "url": "/need-for-drive/static/media/bg_slider_3.a532e517.jpg"
+  },
+  {
+    "revision": "58e1a40d46f93cd9be111f7948a8758a",
+    "url": "/need-for-drive/static/media/image_1.58e1a40d.png"
+  },
+  {
+    "revision": "444f21ad21b27384c41079e4902d2716",
+    "url": "/need-for-drive/static/media/image_2.444f21ad.png"
+  },
+  {
+    "revision": "1e2fee7676ee7836e09aa70ac3690547",
+    "url": "/need-for-drive/static/media/image_3.1e2fee76.png"
+  }
+]);
